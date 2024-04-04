5f176378eca67336fc6706b54dcf658584abab12
Morales <github noreply email@users.noreply.github.com>
2024-04-04 12:20:19 -0600

    new

--- src/hdl/elevator_controller_fsm_tb.vhd
+++ src/hdl/elevator_controller_fsm_tb.vhd
@@ -126,23 +126,24 @@ begin
         
         w_stop <= '1'; wait for k_clk_period * 2;
             assert w_floor = "0100" report "bad wait on floor4" severity failure;  
-        w_stop <= '0'; wait for k_clk_period;
-            assert w_floor = "0100" report "bad up from floor4" severity failure;
+         
+        w_stop <='0'; wait for k_clk_period * 2;
+            assert w_floor = "0100" report "bad wait on floor4" severity failure;
                        
        
 		-- rest of cases
         w_up_down <= '0'; wait for k_clk_period *2;
         -- go back DOWN
        
-        w_stop <= '0'; wait for k_clk_period * 2;
+        w_stop <= '0'; wait for k_clk_period;
           assert w_floor = "0011" report "bad down from floor4" severity failure;
           
 --         w_stop <= '0'; wait for k_clk_period *2;
 --             assert w_floor = "0011" report "bad wait on floor3" severity failure;
-          w_stop <= '0'; wait for k_clk_period * 2;
+          w_stop <= '0'; wait for k_clk_period;
             assert w_floor = "0010" report "bad down from floor3" severity failure; 
 
-        w_stop <= '0'; wait for k_clk_period * 2;
+        w_stop <= '0'; wait for k_clk_period;
           assert w_floor = "0001" report "bad down from floor2" severity failure;
           
        w_stop <= '0'; wait for k_clk_period * 2;
